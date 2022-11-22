4760a7a5fae4d577f65f7a4ad82690459b75b25c
Vadim Ermakov <vadim22.barnaul@gmail.com>
2022-11-22 16:58:36 +0300

    Remove text

--- QtFileGraph/QtFileGraph.cpp
+++ QtFileGraph/QtFileGraph.cpp
@@ -210,23 +210,6 @@ QtFileGraph::drawBackground(QPainter* painter,
     painter->fillRect(rect.intersected(sceneRect), gradient);
     painter->setBrush(Qt::NoBrush);
     painter->drawRect(sceneRect);
-
-    // Text
-    QRectF textRect{sceneRect.left() + 4,
-                    sceneRect.top() + 4,
-                    sceneRect.width() - 4,
-                    sceneRect.height() - 4};
-    QString message{tr("Click and drag the nodes around, and zoom with the mouse "
-                       "wheel or the '+' and '-' keys")};
-
-    auto font = painter->font();
-    font.setBold(true);
-    font.setPointSize(14);
-    painter->setFont(font);
-    painter->setPen(Qt::lightGray);
-    painter->drawText(textRect.translated(2, 2), message);
-    painter->setPen(Qt::black);
-    painter->drawText(textRect, message);
 }
 
 void
